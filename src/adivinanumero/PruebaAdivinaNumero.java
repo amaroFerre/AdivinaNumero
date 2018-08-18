0348b686ec85e30843a2eb77969de86afc7c8816
Amarico <Amarico@EquipoAcer>
2018-08-18 19:52:53 +0200

    Programa en el que tienes que adivinar un numero creado aleatoriamente.
    El programa te irá diciendo si es mayor o menor hasta que lo adivines
    
    Este es el método main

---
+++ src/adivinanumero/PruebaAdivinaNumero.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adivinanumero;
+
+/**
+ *
+ * @author Amarico
+ */
+public class PruebaAdivinaNumero {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        AdivinaNumero adivinar = new AdivinaNumero();
+        
+        adivinar.introduceNombre();
+        
+    }
+    
+    
+}
